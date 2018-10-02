38bf2c09f9f710ca8f82c72f18bfcb61f9dcca01
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-10-01 19:38:28 -0700

    fruit-trees correction

--- algorithms/fruit-trees.py
+++ algorithms/fruit-trees.py
@@ -1,36 +1,30 @@
-# Source: https://leetcode.com/problems/fruit-into-baskets/
-# Author: Shreyas Padhye
-# Algorithm:
-# Percentile: Wrong Answer in [0,1,6,6,4,4,6]
-
 def calculateMaxSelection(A):
     max = 0
     currentSet = []
     currentSet.append(A[0])
     count = 1
     numLast = A[0]
-    countLast = 0
+    countLast = 1
 
     for i in range(1, len(A)):
         if (A[i] in currentSet):
             count += 1
-            if (A[i] == numLast):
-                countLast += 1
-            else:
-                numLast = A[i]
-                countLast = 1
         elif (len(currentSet) != 2):
             currentSet.append(A[i])
             count += 1
-            
-            numLast = A[i]
-            countLast = 1
         else:
             currentSet[0] = A[i-1]
             currentSet[1] = A[i]
             if (count > max):
-                max = count     
+                max = count
             count = 1 + countLast
+        
+        if (A[i] == numLast):
+            countLast += 1
+        else:
+            numLast = A[i]
+            countLast = 1
+#         print("NumLast: ", numLast, " | CountLast: ", countLast)
     if(count > max):
         return count
     else:
@@ -39,4 +33,4 @@ def calculateMaxSelection(A):
 # calculateMaxSelection([1,2,1,3,4,3,5,1,2])   #O: 3
 # calculateMaxSelection([1,2,1,3,3,3,3,1])   #O: 5
 # calculateMaxSelection([1,1,2,2,3,2,2,3])   #O: 6 -> [2,2,3,2,2,3]
-# calculateMaxSelection([0,1,6,6,4,4,6])   #O: 6
+# calculateMaxSelection([0,1,6,6,4,4,6])   #O: 5
