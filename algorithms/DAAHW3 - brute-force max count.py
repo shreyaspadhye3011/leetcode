2ab2412f53e50a0b1d6f073855d991af1d0a209d
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-10-07 16:50:42 -0700

    Added assertions and more test cases to brute-force-max-count

---
+++ algorithms/DAAHW3 - brute-force max count.py
@@ -0,0 +1,31 @@
+# Problem: input: an unsorted array A[lo..hi]; | output: a value that appears most often in the given subarray (ties are broken arbitrarily).
+# Source: SCU COEN279 DAA HW3 Q1
+# Author: Shreyas Padhye
+# Algorithm: Brute Force (given in question). Check all possible cases
+
+def brute_force_max_count(A, S):
+    '''For an unsorted array, this function returns a value that appears most often in a given subarray. In case of ties, it returns the leftmost element from S which has the max count'''
+    assert (type(A) == list and type(S) == list), 'Function expects both parameters to be non-empty lists'
+    assert (len(A) > 0 and len(S) > 0), 'Both main array, A[] & sub array, S[] need to be non-empty'
+    maxItem = A[0]
+    maxCount = 0
+    for currItem in S:
+        currCount = 0
+        for index in range(0, len(A)):
+            if A[index] == currItem:
+                currCount += 1
+        if (currCount > maxCount):
+            maxItem = currItem
+            maxCount = currCount
+    
+    if maxCount > 0:   # case when no element from S[] is found in A[]
+        return maxItem
+    else:
+        return None
+
+brute_force_max_count([1, 2, 3, 4, 2, 3, 3], [2, 3, 1])    #3
+# brute_force_max_count([1, 2, 3, 4, 2, 1, 1], [1, 2])    #1
+# brute_force_max_count([1, 2, 3, 4, 2, 1, 3], [1, 8, 2]) #1
+# brute_force_max_count([1, 2, 3, 4, 2, 1, 3], [7, 8, 5])   #None
+# brute_force_max_count([1, 2, 3, 4, 2, 1, 2], 23)  #assertion error 
+# brute_force_max_count([1, 2, 3, 4, 2, 1, 2], ['as', 4])  #4
--- algorithms/DAAHW3 - transform-conquer max count.py
+++ algorithms/DAAHW3 - transform-conquer max count.py
@@ -1,23 +1,13 @@
 # Problem: input: an unsorted array A[lo..hi]; | output: a value that appears most often in the given subarray (ties are broken arbitrarily).
-# Source: SCU COEN279 DAA HW3
+# Source: SCU COEN279 DAA HW3 Q1
 # Author: Shreyas Padhye
-# Algorithm: Brute Force (given in question). Check all possible cases
+# Algorithm: Transform-Conquer. Sort and check immediate right neighbour as you pass through the array. When neighbour's not a duplicate, update checkItem to current instance
 
 def brute_force_max_count(A, S):
     '''For an unsorted array, this function returns a value that appears most often in a given subarray. In case of ties, it returns the leftmost element from S which has the max count'''
-    # TODO: handle edge cases like A has 0 elements or B has 0 elements or some other type than k=list is passed. Use assert if needed
-    # TODO: check case when nothing in S exists in A, what happens then? should return None
     maxItem = A[0]
     maxCount = 0
-    for currItem in S:
-        currCount = 0
-        for index in range(0, len(A)):
-            if A[index] == currItem:
-                currCount += 1
-        print('Count of ', currItem, ' is: ', currCount)
-        if (currCount > maxCount):
-            maxItem = currItem
-            maxCount = currCount
+    
     
     if maxCount > 0:   # case when no element from S[] is found in A[]
         return maxItem
