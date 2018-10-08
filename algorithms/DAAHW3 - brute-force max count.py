c54ae77d0403b7585e0ddbffdc83ac256034065a
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-10-07 18:20:15 -0700

    transform-conquer created for max_count

--- algorithms/DAAHW3 - brute-force max count.py
+++ algorithms/DAAHW3 - brute-force max count.py
@@ -7,7 +7,7 @@ def brute_force_max_count(A, S):
     '''For an unsorted array, this function returns a value that appears most often in a given subarray. In case of ties, it returns the leftmost element from S which has the max count'''
     assert (type(A) == list and type(S) == list), 'Function expects both parameters to be non-empty lists'
     assert (len(A) > 0 and len(S) > 0), 'Both main array, A[] & sub array, S[] need to be non-empty'
-    maxItem = A[0]
+    maxItem = None
     maxCount = 0
     for currItem in S:
         currCount = 0
@@ -17,11 +17,7 @@ def brute_force_max_count(A, S):
         if (currCount > maxCount):
             maxItem = currItem
             maxCount = currCount
-    
-    if maxCount > 0:   # case when no element from S[] is found in A[]
         return maxItem
-    else:
-        return None
 
 brute_force_max_count([1, 2, 3, 4, 2, 3, 3], [2, 3, 1])    #3
 # brute_force_max_count([1, 2, 3, 4, 2, 1, 1], [1, 2])    #1
