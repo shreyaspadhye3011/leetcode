5d5d9dade24ac18cda60bd78eb07666fbbc2851b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-10-21 22:07:37 -0700

    CTCI - check unique string created with no extra space constraint

--- ctci-solutions/check-unique.py
+++ ctci-solutions/check-unique.py
@@ -3,10 +3,14 @@
 
 # Problem: Given a string, check that it has all unique characters
 
-# 1. Approach: (extra space allowed)  do a single pass and keep storing all items in a dict. If at any point an element already exists in the dict. If all elements passed without break, the string has unique elements
-
+# Solution 1: (extra space allowed): do a single pass and keep storing all items in a dict. If at any point an element already exists in the dict, break. If all elements passed without break, the string has unique elements
+#
+# Solution 2: (NO extra space allowed): do a single pass through the array and at every element A[i] check that the element does not exist in A[:i], if it does break. If all elements passed without break, the string has unique elements
+#
+# Questions to ask:
+#     1. Is the consideration case sensitive? Currently considered so i.e. `a != A`
 class Solution:
-    def check_unique(self, str):
+    def check_unique_extra_space(self, str):
         dict = {}
         count = 0
         for char in str:
@@ -20,7 +24,17 @@ class Solution:
             return True
         else:
             return False
+    
+    def check_unique_no_extra_space(self, str):
+        count = 0
+        for i in range(0, len(str)):
+            if str[i] in str[:i]:
+                break
+        if count == len(str):
+            return True
+        else:
+            return False
 
 object = Solution()
-object.check_unique('abcfd')
+object.check_unique_extra_space('abcfd')
         
