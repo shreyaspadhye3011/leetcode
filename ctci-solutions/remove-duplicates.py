9d145cc06702d55aeb67ff6f5204e1c5890bbf14
Shreyas Padhye <shreyas@Shreyass-MacBook-Pro.local>
2018-11-13 00:47:11 -0800

    ordered dictioanry changes

--- ctci-solutions/remove-duplicates.py
+++ ctci-solutions/remove-duplicates.py
@@ -4,11 +4,14 @@
 # NOTE: One or two additional variables are fine. An extra copy of the array is not. FOLLOW UP Write the test cases for this method
 # Solution Approach: Do a single pass. Create a dictionary to keep track of already passed elements and delete from dictionary if you encounter repeats. Merge dict elements to get string back
 
+# Note: Remember - When you store keys in dictioanry, they are arranged alphabetically
+from collections import OrderedDict   # because simple dictionary changes order to alphabetical implicitly which will disrupt recombined string order and give wrong answer
 class solution():
     def remove_duplicates(self, s):
-        d = {}
+        d = OrderedDict()
 
-        # single pass and delete on duplicate entry
+        # single pass, delete on duplicate entry and add to res string otherwise
+        res = ""
         for char in s:
             if char not in d:
                 d[char] = True
@@ -19,7 +22,7 @@ class solution():
         res = ""
         for val in d:
             res += val
-
+            
         return res
     
 t = solution()
