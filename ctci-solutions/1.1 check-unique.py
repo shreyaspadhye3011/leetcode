3e8436e59d920ee489fd5a135c95e7c8ada81927
Shreyas Padhye <shreyas@Shreyass-MacBook-Pro.local>
2018-11-13 11:17:14 -0800

    corrected file naming

--- ctci-solutions/1.1 check-unique.py
+++ ctci-solutions/1.1 check-unique.py
@@ -4,9 +4,9 @@
 # Problem 1.1 (Pg 94): Given a string, check that it has all unique characters
 
 # Solution 1: (extra space allowed): do a single pass and keep storing all items in a dict. If at any point an element already exists in the dict, break. If all elements passed without break, the string has unique elements
-#
 # Solution 2: (NO extra space allowed): do a single pass through the array and at every element A[i] check that the element does not exist in A[:i], if it does break. If all elements passed without break, the string has unique elements
-#
+# Solution 3: (optimized. no extra space): sort & check previous neighbour for duplicacy in single pass
+
 # Questions to ask:
 #     1. Is the consideration case sensitive? Currently considered so i.e. `a != A`
 class Solution:
