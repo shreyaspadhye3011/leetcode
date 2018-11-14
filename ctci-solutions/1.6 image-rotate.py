568bc1d1127a1b454eccc4a93869495948df4dec
Shreyas Padhye <shreyas@Shreyass-MacBook-Pro.local>
2018-11-13 20:45:14 -0800

    rotate image skeleton

---
+++ ctci-solutions/1.6 image-rotate.py
@@ -0,0 +1,8 @@
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+class solution():
+    def rotate_image(self, M):
+        return
+    
+t = solution()
+t.rotate_image([[1, 2], [3, 4]])
