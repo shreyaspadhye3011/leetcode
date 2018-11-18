39d769aa3182e6f6835539f86193cac3b4f06fbe
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-17 17:52:41 -0800

    CTCI - rotate image code added

--- ctci-solutions/1.6 image-rotate.py
+++ ctci-solutions/1.6 image-rotate.py
@@ -2,7 +2,19 @@
 
 class solution():
     def rotate_image(self, M):
-        return
+        # retrieve order of matrix
+        n = len(M)     
+        m = n          # considered n x n square, as given in question
+
+        # initialize resultant matrix with 0
+        R = [[0 for x in range(m)] for y in range(n)]
+
+        # create 90 degree rotation
+        for row in range(0, n):
+            for col in range(0, n):
+                R[col][n - row - 1] = M[row][col]
+        return R
     
 t = solution()
-t.rotate_image([[1, 2], [3, 4], [2, 7]])
+t.rotate_image([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+t.rotate_image([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
