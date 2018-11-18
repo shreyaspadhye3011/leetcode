508ca07a0876b12e382148c00ceeb7a96a02bb3f
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-17 19:36:17 -0800

    CTCI - in place rotation

--- ctci-solutions/1.6 image-rotate.py
+++ ctci-solutions/1.6 image-rotate.py
@@ -14,7 +14,31 @@ class solution():
             for col in range(0, n):
                 R[col][n - row - 1] = M[row][col]
         return R
+
+    def rotate_in_place(self, matrix):
+        n = len(matrix)
+        for layer in range(0, n/2):
+            first = layer
+            last = n - 1 - layer
+            for i in range(first, last):
+                offset = i - first
+                top = matrix[first][i] # save top
+
+                # left -> top
+                matrix[first][i] = matrix[last-offset][first]
+                
+                # bottom -> left
+                matrix[last-offset][first] = matrix[last][last - offset]
+                
+                # right -> bottom
+                matrix[last][last - offset] = matrix[i][last]
+                
+                # top -> right
+                matrix[i][last] = top # right <- saved top
+        return matrix
     
 t = solution()
-t.rotate_image([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-t.rotate_image([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
+# t.rotate_image([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+t.rotate_in_place([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# t.rotate_image([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
+t.rotate_in_place([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]])
