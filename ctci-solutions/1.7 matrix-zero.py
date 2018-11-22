5969500424b8fa920991766ec5f372d5f18a7a3a
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-22 14:05:10 -0800

    CTCI - 1.7 - test case correction

--- ctci-solutions/1.7 matrix-zero.py
+++ ctci-solutions/1.7 matrix-zero.py
@@ -32,3 +32,6 @@ class solution():
 t = solution()
 t.matrix_zero_extra_space([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 t.matrix_zero_extra_space([[0, 2, 3], [4, 5, 6], [7, 8, 9]])
+t.matrix_zero_extra_space([[0, 2, 3], [4, 5, 6], [7, 8, 0]])
+t.matrix_zero_extra_space([[0, 2, 3, 1], [4, 0, 6, 7], [7, 8, 0, 1], [3, 6, 7, 8]])
+t.matrix_zero_extra_space([[3, 2, 3, 1], [4, 0, 6, 7], [7, 8, 1, 1], [3, 6, 7, 8]])
