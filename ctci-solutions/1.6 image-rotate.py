7b140d7deda64c6ec1dbe1e6884c6ffe73c9e201
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-22 14:07:36 -0800

    added authoring

--- ctci-solutions/1.6 image-rotate.py
+++ ctci-solutions/1.6 image-rotate.py
@@ -1,3 +1,6 @@
+# Reference: CTCI. Pg 48
+# Author: Shreyas Padhye
+
 # Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
 class solution():
--- ctci-solutions/1.7 matrix-zero.py
+++ ctci-solutions/1.7 matrix-zero.py
@@ -1,3 +1,6 @@
+# Reference: CTCI. Pg 48
+# Author: Shreyas Padhye
+
 # Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
 # column is set to 0.
 
