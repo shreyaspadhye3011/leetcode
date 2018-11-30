2df25b08a371ba29f51244e175233fc4f012b985
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-30 12:45:17 -0800

    CTCI 1.8 - added corrections

--- ctci-solutions/1.8 string-rotation.py
+++ ctci-solutions/1.8 string-rotation.py
@@ -2,17 +2,25 @@
 
 class solution():
     def check_string_rotation(self, s1, s2):
-        if s2 in (s1 +""+ s1):
-            return True
+        original_len = len(s1)
+#         return "aa" in "aad"
+        if original_len == len(s2):
+            original_str = s1
+            s1 += s1
+            i = s1.find(s2)
+            s1 = s1[:i] + s1[i+original_len:]
+            print(s1)
+            if s1 == original_str:
+                return True
         return False
 
-obj = solution()
-obj.check_string_rotation("waterbottle", "erbottlewat")    # True
-obj.check_string_rotation("waterbottle", "rbottlewat")     # False
-obj.check_string_rotation("waterbottle", "rbottlewat")     # False
-obj.check_string_rotation("aabc", "aabc")     # True
+# obj = solution()
+# obj.check_string_rotation("waterbottle", "erbottlewat")    # True
+# obj.check_string_rotation("waterbottle", "rbottlewat")     # False
+# obj.check_string_rotation("waterbottle", "rbottlewat")     # False
+# obj.check_string_rotation("aabc", "aabc")     # True
 obj.check_string_rotation("abAc", "abac")     # False
-obj.check_string_rotation("abac", "aabc")     # False
-obj.check_string_rotation("kklmo", "klmo")     # False
-obj.check_string_rotation("kkkk", "kkkk")     # True
-obj.check_string_rotation("akkb", "bakk")     # True
+# obj.check_string_rotation("abac", "aabc")     # False
+# obj.check_string_rotation("kklmo", "klmo")     # False
+# obj.check_string_rotation("kkkk", "kkkk")     # True
+# obj.check_string_rotation("akkb", "bakk")     # True
