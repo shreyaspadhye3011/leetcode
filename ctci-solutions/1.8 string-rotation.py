35f1318c8997b9bcea2bd4aea65a0b7f8dfb09f1
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-29 22:30:38 -0800

    Added initial test case

--- ctci-solutions/1.8 string-rotation.py
+++ ctci-solutions/1.8 string-rotation.py
@@ -1,1 +1,8 @@
 # Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”).
+
+class solution():
+    def check_string_rotation(self, s1, s2):
+        return True
+
+obj = solution()
+obj.check_string_rotation("waterbottle", "erbottlewat")     # true
