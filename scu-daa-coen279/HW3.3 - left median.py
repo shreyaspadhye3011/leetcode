2b3ed18cdf9176342f0dc9fe9c696c1a2fc75122
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-30 18:33:42 -0800

    Left Median - Added test cases

--- scu-daa-coen279/HW3.3 - left median.py
+++ scu-daa-coen279/HW3.3 - left median.py
@@ -3,24 +3,34 @@
 # Author: Shreyas Padhye
 # Algorithm: Decrease-Conquer
 
-def left_median(A):
-    if len(A) == 1 or len(A) == 2:
-        return A[0]
-    else:
-        median = left_median(A[:-1])
-        if len(A[:-1]) % 2 == 0:
-            if median > A[-1]:
+class solution():
+    def left_median(self, A):
+        if len(A) == 1 or len(A) == 2:
+            return A[0]
+        else:
+            median = self.left_median(A[:-1])
+            if len(A[:-1]) % 2 == 0:
+                if median > A[-1]:
+                    return median
+                else:
+                    med_neighbour = A.index(median) + 1
+                    if A[-1] < A[med_neighbour]:
+                        temp = A[-1]
+                        A[-1] = A[med_neighbour]
+                        A[med_neighbour] = temp
+                    return A[med_neighbour]
+            elif median < A[-1]:
                 return median
             else:
-                med_neighbour = A.index(median) + 1
-                if A[-1] < A[med_neighbour]:
+                med_neighbour = A.index(median) - 1
+                if A[-1] > A[med_neighbour]:
                     temp = A[-1]
                     A[-1] = A[med_neighbour]
                     A[med_neighbour] = temp
                 return A[med_neighbour]
-        elif median < A[-1]:
-            return median
-        else:
-        
 
-        return
+t = solution()
+# t.left_median([0, 1, 3, 5])   #1
+# t.left_median([0, 1, 3, 5, 2])   #2
+# t.left_median([0, 1, 3, 5])   #1
+t.left_median([0, 1, 3, 5])   #1
