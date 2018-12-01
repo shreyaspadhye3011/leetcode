8505bd298466878441b892788cd16be23d4d5a01
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-11-30 17:53:09 -0800

    SCU DAA - Added left median init code

--- scu-daa-coen279/HW3.3 - left median.py
+++ scu-daa-coen279/HW3.3 - left median.py
@@ -7,4 +7,20 @@ def left_median(A):
     if len(A) == 1 or len(A) == 2:
         return A[0]
     else:
-        return merge
+        median = left_median(A[:-1])
+        if len(A[:-1]) % 2 == 0:
+            if median > A[-1]:
+                return median
+            else:
+                med_neighbour = A.index(median) + 1
+                if A[-1] < A[med_neighbour]:
+                    temp = A[-1]
+                    A[-1] = A[med_neighbour]
+                    A[med_neighbour] = temp
+                return A[med_neighbour]
+        elif median < A[-1]:
+            return median
+        else:
+        
+
+        return
