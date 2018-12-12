bad9aa8de3e34e315b541806c3f426dbf9b1a223
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2018-12-11 16:10:26 -0800

    4. sorted median -  construction of generic code

--- algorithms/4. sorted median.py
+++ algorithms/4. sorted median.py
@@ -6,18 +6,26 @@ B = [7, 8, 9, 10, 11]
 len_A = len(A)
 len_B = len(B)
 
-# consider A to always have the smaller array. If not, swap
+# consider A to always have the smaller array. If not, swap -- nay not be required in new algo. Check
 if len_B < len_A:
     temp = A
     A = B
     B = temp
 
-# case A: when the smaller array that is appended (A) has all elements smaller to the larger array
-if A[-1] < B[0]:
-#     median_offset = float(len_A) / 2
-    median_offset = len_A / 2.0
-    
+# find both medians
+median_A = statistics.median(A)
+median_B = statistics.median(B)
+# not needed (just kept to show that this can be done in constant time if library usage discouraged): 
+# if len_A % 2 != 0:
+#     median_A = A[len_A / 2]
+# else:
+#     median_A = (A[(len_A / 2) - 1] + A[len_A / 2]) / 2.0
+
+# if len_B % 2 != 0:
+#     median_B = B[len_B / 2]
+# else:
+#     median_B = (B[(len_B / 2) - 1] + B[len_B / 2]) / 2.0
+
 C = A + B
 print(C)
 print(statistics.median(C))
-print(median_offset)
