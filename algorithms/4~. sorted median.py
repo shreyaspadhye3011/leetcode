77646fb4c5b239480d3cac148470e7126d6660de
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-01-12 06:32:05 -0800

    sorted median temp

--- algorithms/4~. sorted median.py
+++ algorithms/4~. sorted median.py
@@ -1,3 +1,4 @@
+# TODO: Complete code
 # pre-requisite: Both arrays are sorted, given
 import statistics
 # A = [2, 3]
@@ -33,3 +34,25 @@ median_B = statistics.median(B)
 C = A + B
 print(C)
 print(statistics.median(C))
+
+# Approach: Split the 2 arrays in two sections such that both sections have equal number of elements and every element on the left side is smaller than the right side. Start from a mid slice and move left / right based on the condition observed
+
+class solution():
+    def find_sorted_median(self, A, B):
+        len_A = len(A)
+        len_B = len(B)
+
+        # step 1: consider A to always have the smaller array. If not, swap -- nay not be required in new algo. Check
+        if len_B < len_A:
+            temp = A
+            A = B
+            B = temp
+
+        start = 0
+        end = len_A - 1
+        left_x = (start + end) / 2 
+
+        return True
+
+obj = solution()
+obj.find_sorted_median([1,3], [2,4])
