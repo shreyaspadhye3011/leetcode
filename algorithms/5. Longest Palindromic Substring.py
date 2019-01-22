fade92942e317dfff8bd5ad8612a4c6aedab0d28
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-01-22 08:16:27 -0800

    Leetcode 5. - test code 2

--- algorithms/5. Longest Palindromic Substring.py
+++ algorithms/5. Longest Palindromic Substring.py
@@ -11,7 +11,25 @@ class Solution:
         :rtype: str
         """
         s = "strings"
-        s[0] + s[-1]
-        s[1] + s[-2]
-        s[2] + s[-3]
-        s[3] + s[-4] 
+        # s[0] + s[-1]
+        # s[1] + s[-2]
+        # s[2] + s[-3]
+        # s[3] + s[-4]
+        # s[-1:]
+
+        max_palindrome = 0
+        for i in range(len(s)):
+            temp = len(s[i:])
+            mid = len / 2
+            inner = 0
+            while inner < mid:
+                back_index = -(i+1)
+                if temp[i] == temp[back_index]:
+                    inner += 1
+                else:
+                    break
+            if inner == mid:
+                if len(temp) > max_palindrome:
+                    max_palindrome = len(temp)
+
+        max_palindrome
