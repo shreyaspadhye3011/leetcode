9d9407bce96ba28b641ebbf5044bfd33ed087c9b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-01-22 07:45:00 -0800

    Leetcode 5 - test code

---
+++ algorithms/5. Longest Palindromic Substring.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+# Status: 
+# Percentile: 
+
+# Approach: 
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = "strings"
+        s[0] + s[-1]
+        s[1] + s[-2]
+        s[2] + s[-3]
+        s[3] + s[-4] 
