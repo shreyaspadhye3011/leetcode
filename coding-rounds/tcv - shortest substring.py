72cd0e953a033e2a635441cfa4f61e8ec2bb40ce
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-04-07 23:42:02 -0700

    added interview questions

---
+++ coding-rounds/tcv - shortest substring.py
@@ -0,0 +1,16 @@
+def smallest_substr(str):
+    length = len(str)
+    min = length
+    for i in range(0, length - 1):
+        j = i
+        while j <= length:
+            if set(str[i:j]) == set(str):
+                if len(str[i:j]) < min:
+                    min = len(str[i:j])
+            j += 1
+    return min
+
+# smallest_substr('abaacbab') #3
+# smallest_substr('bcaacbc')  #3
+# smallest_substr('aabaaccbcbcca')  #4
+smallest_substr('bbcbbdbccccabbbbd')  #7
