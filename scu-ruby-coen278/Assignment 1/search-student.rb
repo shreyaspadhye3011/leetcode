2a6551d378b881e400a2dd867e9ca79986a57be8
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-04-25 18:53:47 -0700

    added hash based searching

--- scu-ruby-coen278/Assignment 1/search-student.rb
+++ scu-ruby-coen278/Assignment 1/search-student.rb
@@ -1,9 +1,26 @@
 class SearchStudent
-    def search_students(students, name)
+    def search_students2(students, name)
         @students = students
         found = false
         @students.each do |student|
           if student[:firstname].downcase.eql?(name.downcase)
+            if (!found)
+                puts "First Name\tLast Name\t\tPhone#"
+            end
+            found = true
+            puts "#{student[:firstname]}\t\t#{student[:lastname]}\t\t#{student[:phonenumber]}"
+          end
+        end
+        if !(found)
+            puts "No record found"
+        end
+    end
+
+    def search_students(students, hash)
+        @students = students
+        found = false
+        @students.each do |student|
+          if student[hash.keys[0]].downcase.eql?(hash.values[0].downcase)
             if (!found)
                 puts "First Name\tLast Name\t\tPhone#"
             end
@@ -24,4 +41,7 @@ students = [
 {:firstname => "Marisa", :lastname => "lastnamemarisa",  :phonenumber => 443234567},
 {:firstname => "Ken", :lastname => "Kenlastname",  :phonenumber => 456734244}
 ]
-SearchStudent.new.search_students(students, "ken")
+# SearchStudent.new.search_students(students, firstname: "ken")
+# SearchStudent.new.search_students(students, firstname: "KEn")
+SearchStudent.new.search_students(students, firstname: "KEny")
+
