629bbab6b9a80489caa216000eac526dd85e7663
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-04-25 22:28:48 -0700

    merge hash changes

--- scu-ruby-coen278/Assignment 1/merge-hash.rb
+++ scu-ruby-coen278/Assignment 1/merge-hash.rb
@@ -2,24 +2,24 @@ class Hash
     def merge(h2)
         res = dup
         h2.each do |key, val|
-            res[key] = if block_given? && key?(key)
-                            yield(key, res[key], val)
-                        else
-                            val
-                        end
+            if block_given? && key?(key)
+                res[key] = yield(key, res[key], val)
+            else
+                res[key] = val
+            end
         end
-        res
+        return res
     end
   
     def merge!(h2)
         h2.each do |key, val|
-        self[key] = if block_given? && key?(key)
-                      yield(key, self[key], val)
-                    else
-                      val
-                    end
+            if block_given? && key?(key)
+                self[key] = yield(key, self[key], val)
+            else
+                self[key] = val
+            end
         end
-        self
+        return self
     end
 end
 
@@ -27,5 +27,7 @@ end
 h1 = { 'a' => 100, 'b' => 200 }
 h2 = { 'b' => 254, 'c' => 300 }
 # p h1.merge(h2) { |_key, val1, val2| val2 - val1 }
-p h1.merge(h2) 
-  
+# p h1.merge(h2) 
+# p h1
+p h1.merge!(h2)
+p h1
