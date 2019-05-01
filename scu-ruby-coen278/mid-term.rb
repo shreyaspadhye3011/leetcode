9ec249f8bcff802007ec40c06a798387dc805d77
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-01 00:28:59 -0700

    ruby mid term sample

---
+++ scu-ruby-coen278/mid-term.rb
@@ -0,0 +1,12 @@
+# module M          
+#     module N
+#         class C
+#              class D                
+#              end
+#             CONT = 10          
+#         end
+#     end
+# end
+
+# puts M::N::C::CONT
+
