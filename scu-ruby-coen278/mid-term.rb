a7ae6b6bef1bd14fadaf6325ca1d9fbff44f1681
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-01 00:47:00 -0700

    completed sample mid term

--- scu-ruby-coen278/mid-term.rb
+++ scu-ruby-coen278/mid-term.rb
@@ -1,3 +1,4 @@
+# Q1
 # module M          
 #     module N
 #         class C
@@ -7,6 +8,27 @@
 #         end
 #     end
 # end
-
 # puts M::N::C::CONT
 
+# Q2
+# puts /love/  =~ "love you"
+# puts /^you/ =~ "love you"
+# puts /[aeiou]/ =~ "love you"
+# puts /./ =~ "love"
+
+# Q3
+# x = 1
+# x *= 3 while x < 40
+# x -= 6 until x < 60
+# puts x
+
+# Q4
+# people = [
+#   {:name => "bob", :age => 42},
+#   {:name => "frank", :age => 13}
+# ]
+# arr = []
+# people.each do |peo|
+#     arr.push(peo[:name])
+# end
+# p arr
