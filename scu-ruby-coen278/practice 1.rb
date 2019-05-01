a9010a66edf37a343b47886effab368f396440a9
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-01 01:17:29 -0700

    practice paper push

---
+++ scu-ruby-coen278/practice 1.rb
@@ -0,0 +1,25 @@
+# puts 11.even?
+# puts 11.odd?
+# puts 11.class
+# puts 123456789012345678901234567890.class
+# puts 11.next
+# puts 11.succ
+
+# puts 12.9.ceil
+# puts (-12.9).ceil
+# puts -12.9.abs
+# puts 12.9.floor
+# puts 12.9.to_i
+# puts 12.9.to_int
+# puts 10 ** 2
+# puts 12.9.round
+# puts 12.4.round
+# puts 12.5.round
+# puts 3.14159.round(2)
+# puts 4.14159.round(4)
+# puts 4.14152.round(3)
+
+a = 10
+a.times { |x|  puts x}
+a.times { |x|  print x}
+
