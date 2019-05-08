9408c1ba2e8db881d2ec50d031a18f3c17df201b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-08 00:49:25 -0700

    Completed stacks in array code

--- ctci-solutions/3.1 stacks-in-array.py
+++ ctci-solutions/3.1 stacks-in-array.py
@@ -6,19 +6,19 @@ class stackinarr:
         self.A = A 
 
     def push1(self, item):
-        self.A[0].append = [item]
+        self.A[0].append(item)
 
     def pop1(self):
         self.A[0].pop()
 
     def push2(self, item):
-        self.A[1].append = [item]
+        self.A[1].append(item)
 
     def pop2(self):
         self.A[1].pop()
 
     def push3(self, item):
-        self.A[2].append = [item]
+        self.A[2].append(item)
 
     def pop3(self):
         self.A[2].pop()
@@ -28,5 +28,17 @@ class stackinarr:
 
 obj = stackinarr()
 obj.push1("a")
+obj.push1("b")
+obj.push1("c")
+obj.push2("b")
+obj.pop1()
+obj.push2("b")
+obj.push2("c")
+obj.push2("c")
+obj.push3("p")
+obj.push3("q")
+obj.pop3()
+obj.pop2()
+obj.show()
     
 
