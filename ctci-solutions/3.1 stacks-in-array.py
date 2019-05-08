565411dabebb9356391715b0d826241c57573963
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-08 00:44:30 -0700

    added show finction

--- ctci-solutions/3.1 stacks-in-array.py
+++ ctci-solutions/3.1 stacks-in-array.py
@@ -1,5 +1,6 @@
 # Problem: Implement 3 stacks in one array
 # Solution: Create push, pop, length functions for all 3 stacks and this should solve our purpose
+
 class stackinarr:
     def __init__(self, A=[[], [], []]):
         self.A = A 
@@ -21,5 +22,11 @@ class stackinarr:
 
     def pop3(self):
         self.A[2].pop()
+
+    def show(self):
+        return self.A
+
+obj = stackinarr()
+obj.push1("a")
     
 
