04b8f7eb5c282727ab37a69c17467ee83e852e20
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-05-08 16:32:45 -0700

    SetOfStacks push created

---
+++ ctci-solutions/3.3 SetOfStacks.py
@@ -0,0 +1,18 @@
+# Imagine a (literal) stack of plates If the stack gets too high, it might topple Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold Implement a data structure SetOfStacks that mimics this SetOfStacks should be composed of several stacks, and should create a new stack once the previous one exceeds capacity SetOfStacks push() and SetOfStacks pop() should behave identically to a single stack (that is, pop() should return the same values as it would if there were just a single stack)
+# FOLLOW UP: Implement a function popAt(int index) which performs a pop operation on a specific sub-stack
+
+class SetOfStacks:
+    def __init__(self, A=[[]], currentPos=0, limit=10):
+        self.A = A
+        self.currentPos = currentPos
+        self.limit = limit
+    
+    def push(self, item):
+        if len(self.A[self.currentPos] > self.limit):
+            self.A.append([item])
+            self.currentPos += 1
+        else:
+           self.A[self.currentPos].append(item)
+    
+    def show(self):
+        return self.A
