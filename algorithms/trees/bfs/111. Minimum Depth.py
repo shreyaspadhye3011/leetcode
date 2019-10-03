8159cc234d64470b2acae24df8096e13331b9758
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-10-02 18:29:14 -0700

    working example 1 of min_depth

--- algorithms/trees/bfs/111. Minimum Depth.py
+++ algorithms/trees/bfs/111. Minimum Depth.py
@@ -1,9 +1,5 @@
-# Reference: https://leetcode.com/problems/minimum-depth-of-binary-tree/
-
-# Problem: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
-# Approach: Modify BFS to a depth based retrieval using dictionary: (either the final result can be depth based dictionary or access can be turned into a dictionary) + add mode parameter to keep track of order for zigzag
-# TODO: Add zigzag & depth specific retrieval logic
-
+# Problem: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+# Approach: on None, return. Keep track of min_depth all throughout. Once processed, min_depth will have answer
 class Node:
     def __init__(self, data):
         self.left = None
@@ -17,49 +13,17 @@ class Node:
         print(self.data)
         if self.right:
             self.right.PrintTree()
-    
-    def getChildren(self, mode):
-        # update to facilitate for graphs. Currently made for trees. For graphs, it will be done through adjacency list
-        if self.left and self.right:
-            if mode == "left":
-                return [self.left, self.right]
-            else:
-                return [self.right, self.left]
-        elif self.left:
-            return [self.left]
-        elif self.right:
-            return [self.right]
-        else:
-            return None
-
 
 class Solution():
-    def zigZagOrder(self, root):
-        access = [root]
-        result = [[root.data]]
-        depth_dict = {}
-        depth_dict[root] = 0
-        mode = "left"
-        while(len(access) > 0):
-            node = access.pop(0)
-            #TODO: update mode to be dynamic in call
-            depth = depth_dict[node] + 1
-            childList = node.getChildren(mode)
-            if childList:
-                for child in childList:
-                    access.append(child)
-                    depth_dict[child] = depth
-                    # append to result array when first entry of depth level
-                    if depth == len(result): 
-                        result.append([child.data])
-                    # append at depth when entry already exists
-                    else:
-                        if depth % 2 != 0:
-                            result[depth].append(child.data)
-                        else:
-                            # TODO: check whether creating list and then reversal is better than this (check complexity of insert or can even do `7 + [1, 2, 3]``)
-                            result[depth].insert(0, child.data)
-        return result
+    def minDepth(self, root):
+        if root.left and root.right:
+            return min(1 + self.minDepth(root.left), 1 + self.minDepth(root.right))
+        elif root.left:
+            return 1 + self.minDepth(root.left)
+        elif root.right:
+            return 1 + self.minDepth(root.right)
+        else:
+            return 0
 
 # Use the insert method to add nodes
 # root = Node(12)
@@ -71,9 +35,9 @@ class Solution():
 root = Node(1)
 # depth: 1
 root.left = Node(4)
+# depth: 2
 root.left.left = Node(5)
 root.left.right = Node(6)
-# Output: [[1], [4], [6, 5]]
 
 obj = Solution()
-obj.zigZagOrder(root)
+obj.minDepth(root)
