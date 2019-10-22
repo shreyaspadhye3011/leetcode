5619a05e99968d053f2159ddb14de270717cd1da
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-10-21 23:33:38 -0700

    added business logic

--- algorithms/trees/bfs/200. Number of islands.py
+++ algorithms/trees/bfs/200. Number of islands.py
@@ -2,17 +2,32 @@
 
 class Solution(object):
     islandCount = 0
-    continuation = False
     visited_dict = {}
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
+        self.recurseNumIslands(self, grid, False)
+
+    def recurseNumIslands(self, grid, continuation):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         for row in range(len(grid)):
             for col in range(len(grid[row])):
-                print(row, col)
-                
+                if (row, col) not in self.visited_dict:
+                    self.visited_dict[(row, col)] = 'visited'
+                    if grid[row][col] == 1 and continuation == False:
+                        self.islandCount += 1
+                        continuation = True
+                    
+                    elif grid[row][col] == 0:
+                        continuation = False
+                    # todo: call neighbors recursively with their respective value of continuation
+
+        # print(self.visited_dict)      
     
 obj = Solution()
 obj.numIslands([[1,1,1,1,0],[1,1,0,1,0],[1,1,0,0,0],[0,0,0,0,0]])
