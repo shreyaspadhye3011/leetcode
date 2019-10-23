2a8c1cdadc8b40733afbc829c7da9c204fe39b53
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-10-23 12:01:21 -0700

    Completed iteration 1

--- algorithms/trees/bfs/200. Number of islands.py
+++ algorithms/trees/bfs/200. Number of islands.py
@@ -8,24 +8,34 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        self.recurseNumIslands(self, grid, False)
+        rowLen = len(grid)
+        colLen = len(grid[0])       # considering symmetric matrix. Also add defensive condition on index 0 access
+        self.recurseNumIslands(self, grid, 0, 0, rowLen, colLen, False)
+        return self.islandCount
 
-    def recurseNumIslands(self, grid, continuation):
+    def recurseNumIslands(self, grid, row, col, rowLen, colLen, continuation):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        for row in range(len(grid)):
-            for col in range(len(grid[row])):
-                if (row, col) not in self.visited_dict:
-                    self.visited_dict[(row, col)] = 'visited'
-                    if grid[row][col] == 1 and continuation == False:
-                        self.islandCount += 1
-                        continuation = True
-                    
-                    elif grid[row][col] == 0:
-                        continuation = False
-                    # todo: call neighbors recursively with their respective value of continuation
+        # rowLen = len(grid)
+        # for row in range(rowLen):
+        #     colLen = len(grid[row])
+        #     for col in range(colLen):
+        if (row, col) not in self.visited_dict:
+            self.visited_dict[(row, col)] = 'visited'
+            if grid[row][col] == 1 and continuation == False:
+                self.islandCount += 1
+                continuation = True
+            
+            elif grid[row][col] == 0:
+                continuation = False
+
+            # todo: call neighbors recursively with their respective value of continuation
+            if row + 1 < rowLen:
+                self.recurseNumIslands(grid, row + 1, col, rowLen, colLen, continuation)
+            if col + 1 < colLen:
+                self.recurseNumIslands(grid, row, col + 1, rowLen, colLen, continuation)
 
         # print(self.visited_dict)      
     
