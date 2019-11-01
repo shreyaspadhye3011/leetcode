42b228c28b5b217a9fe9a96d2e9aa91eaae0821f
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-10-31 18:00:04 -0700

    added new approach to solutiom

--- algorithms/trees/bfs/200. Number of islands.py
+++ algorithms/trees/bfs/200. Number of islands.py
@@ -5,24 +5,48 @@
 # 3. The final value of current_island_count is the answer
 
 class Solution(object):
-    islandCount = 0
-    land_locations = []
+    grid = []
     visited_dict = {}
+    rowLen = 0
+    colLen = 0
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        # rowLen = len(grid)
-        # colLen = len(grid[0])       # considering symmetric matrix. Also add defensive condition on index 0 access
-        # self.recurseNumIslands(self, grid, 0, 0, rowLen, colLen, False)
-        # return self.islandCount
-        rowLen = len(grid)
-        colLen = len(grid[0])       # considering symmetric matrix. Also add defensive condition on index 0 access
-        for row in range(rowLen):
+        self.grid = grid
+        islandCount = 0
+        land_locations = []
+        self.rowLen = len(grid)
+        self.colLen = len(grid[0])       # considering symmetric matrix. Also add defensive condition on index 0 access
+
+        for row in range(self.rowLen):
             for col in range(colLen):
                 if grid[row][col] == 1:
                     land_locations.append((row, col))
+        
+        for (row, col) in land_locations:
+            # if already visited, do not recurse on marking
+            if (row, col) not in self.visited_dict:
+                islandCount += 1
+                # self.visited_dict[(row, col)] = islandCount
+                # mark self and neighbours
+                self.markNeighbours(row, col, islandCount)
+                # if row + 1 < rowLen:
+                #     self.markNeighbours(row + 1, col, islandCount)
+                # if col + 1 < colLen:
+                #     self.markNeighbours(row, col + 1, islandCount)
+        return islandCount
+
+    def markNeighbours(self, row, col, islandCountMarker):
+        # first mark self
+        self.visited_dict[(row, col)] = islandCountMarker
+        # check whether neighbours exist and mark if it's a piece of connected land 
+        if row + 1 < self.rowLen and self.grid[row + 1][col] == 1:
+            self.markNeighbours(row + 1, col, islandCountMarker)
+        if col + 1 < self.colLen and self.grid[row][col + 1] == 1:
+            self.markNeighbours(row, col + 1, islandCountMarker)
+
 
     def recurseNumIslands(self, grid, row, col, rowLen, colLen, continuation):
         """
