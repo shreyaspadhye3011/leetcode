4c2efb9d96326c6533adbaebd3d79a5861b57721
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-11-09 15:06:14 -0800

    improved test cases

--- algorithms/trees/dfs/isSameTree.py
+++ algorithms/trees/dfs/isSameTree.py
@@ -5,13 +5,15 @@ class Solution:
         :type q: TreeNode
         :rtype: bool
         """
+        
         if not p and not q:
             return True
         if not q or not p:
             return False
         if p.val != q.val:
             return False
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        print("Callig for:"+str(p.val)+" and "+str(q.val))
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
 class Node:
     def __init__(self, data):
@@ -22,16 +24,16 @@ class Node:
 root = Node(1)
 root.left = Node(4)
 root.right = Node(5)
-root.left.left = Node(4)
-root.left.right = Node(4)
-root.right.right = Node(5)
+root.left.left = Node(6)
+root.left.right = Node(7)
+root.right.right = Node(15)
 
 root2 = Node(1)
 root2.left = Node(4)
 root2.right = Node(10)
-root2.left.left = Node(4)
-root2.left.right = Node(4)
-root2.right.right = Node(5)
+root2.left.left = Node(6)
+root2.left.right = Node(7)
+root2.right.right = Node(15)
 
 
 obj = Solution()
