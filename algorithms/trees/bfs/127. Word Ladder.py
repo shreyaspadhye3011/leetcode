7f3e68f432c6ee77945de11bb49fba9ff1d798b7
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-11-10 01:05:02 -0800

    word ladder update

--- algorithms/trees/bfs/127. Word Ladder.py
+++ algorithms/trees/bfs/127. Word Ladder.py
@@ -7,18 +7,34 @@
 ############
 
 # Approach 2: Create a dictionary that stores all possible one letter variations from the wordlist given. Do a BFS like algo. For every word from the access list, get all words from it's "one word varitaion" dictionary key and put them in access list. Return count when endWord is found in access list
-
-class Solution(object):
+class solution:
+    dict = {}
     def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
+        self.createDictionary(wordList)
         access = [beginWord]
         count = 0
+        accessed = []
         while len(access) != 0:
-            node = access.pop()
-            if node == endWord:
+            currWord = access.pop()
+            if currWord == endWord:
                 return count
+            count += 1
+            accessed.append(currWord)
+            for iword in self.getRegEx(currWord):
+                for rword in self.dict.setdefault(iword, []):
+                    if rword not in accessed:
+                        access.append(rword)
+        return 0
+    
+    def createDictionary(self, wordList):
+        for word in wordList:
+            for iword in self.getRegEx(word):
+                self.dict.setdefault(iword, []).append(word)
+
+    def getRegEx(self, word):
+        pattern = []
+        for i in range(len(word)):
+            temp = word
+            temp[i] = '*'
+            pattern.append(temp)
+        return pattern
