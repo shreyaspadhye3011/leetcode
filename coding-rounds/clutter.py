5705cda2180ed34cf000ca69aaabe903f2591d7e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-11-28 17:05:46 -0800

    Added first few parts os clutter

---
+++ coding-rounds/clutter.py
@@ -0,0 +1,8 @@
+# Keep on Trucking - check the tasks to be completed by a truck and return whether it is blocked or not
+# is_blocked(required_tasks, task_from, task_to)
+# required_tasks -> list of tasks that are mandatory to complete
+# ith index of task_to depends on ith index of task_from
+
+# Hint:
+# Unless there is an explicit from -> to task
+
