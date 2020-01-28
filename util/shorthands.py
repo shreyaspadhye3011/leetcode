ae0f3fa23eb0a5afbf9d10816d397a5c2b61d24a
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-01-28 00:38:47 -0800

    Added more shorthands

--- util/shorthands.py
+++ util/shorthands.py
@@ -1,3 +1,8 @@
+# lists & sort
+a = [5, 5, 1, 7, 9, 1]
+list(set(a))    # remove redundant elements. Note: will automatically sort
+sorted(a)       # sort the list
+
 # for in reversed
 for i in reversed(range(5)):
     print(i)    # 4,3,2,1,0
@@ -8,22 +13,6 @@ for i in range(10, 4, -2):  # start, stop, step :: until [element > stop]
 
 # list comprehension
 
-# mapping
-[i * 2 for i in range(10) if i * 2 > 4]
-# o: [6, 8, 10, 12, 14, 16, 18]
-
-# conditional
-print("Safe" if 2 < 1 else "Unsafe")
-# o: Unsafe
-
-# lambda
-# TODO
-
-# map
-# TODO
-
-# filter
-# TODO
 
 # lists
 a = ['a', 'b', 'c', 'd']    # implode lists or join
@@ -49,3 +38,30 @@ s = [(k, d[k]) for k in sorted(d, key=d.get, reverse=True)]
 # character manipulation
 ch = 'a'
 x = chr(ord(ch) + 2) # x -> c
+
+# convert an integer number to binary
+"{0:b}".format(9) # O: 1001 : Useful when questions like create a superset of an array eg. [2,3,4] : [[2], [3], [4], [2,3], [3,4], [2,4], [2,3,4], None] i.e 8 sets and you can use binary representations from 1 (001) -> 7 (111) to get all these sets. Although the complexity will be 2^n 
+
+# convert binary to int
+int('101', 2)   # O: 5
+
+# get all subsets of a set of size n
+import itertools
+list(itertools.combinations([1, 2, 3], 2)) # get all subsets with 2 elements
+
+# mapping
+[i * 2 for i in range(10) if i * 2 > 4]
+# o: [6, 8, 10, 12, 14, 16, 18]
+
+# conditional
+print("Safe" if 2 < 1 else "Unsafe")
+# o: Unsafe
+
+# lambda
+# TODO
+
+# map
+# TODO
+
+# filter
+# TODO
