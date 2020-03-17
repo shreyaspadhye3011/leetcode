318fccdd16887d363b6d503564381cb9a642455c
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-03-17 13:30:38 -0700

    Added set sort

--- util/shorthands.py
+++ util/shorthands.py
@@ -9,6 +9,22 @@ a = [5, 5, 1, 7, 9, 1]
 list(set(a))    # remove redundant elements. Note: will automatically sort
 sorted(a)       # sort the list
 
+# O(1) search Time! -- use Set instead of lists
+# sets and disctionary
+# imp: search in set is O(1) just like a dictionary. Dictionary's retrieval is also O(1)
+my_set = {1, 2, 3}    # initializing a non-empty set
+3 in my_set         # True   
+my_set.add(4)       # add element. Remember: set does not store duplicates
+my_set.remove(2)    # remove an element
+a = set()           # initialziing empty set
+# IMP:
+b = [7, 4, 5, 6, 5]
+my_set = set(b)     # creating a set from a list # O: {4, 5, 6, 7} 
+# Note that the conversion to set also sorts the elements! Also this happens in O(n) time. Iterating through each element and each to a hash set: https://stackoverflow.com/questions/34642155/what-is-time-complexity-of-a-list-to-set-conversion
+# i.e. you can sort in O(n) using this concept! (possibly Radix sort type working) -- basically it creates a hash entry for every element -- But can practically be used only for limited size of arrays as after a while it will lead to collision in hash key and it will no longer be a constant access time
+# Conclusion: can try this method for small test cases
+
+
 # for in reversed
 for i in reversed(range(5)):
     print(i)    # 4,3,2,1,0
