a09a7baee184e19478e7243a8ab983a79f30d993
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-04-11 14:23:23 -0700

    added more JS shorthands

--- util/shorthands.js
+++ util/shorthands.js
@@ -1,3 +1,22 @@
+// Timeout: call a function after a delay
+setTimeout(function(){
+  // code to exdcute after delay
+}, 3000); 
+
+// Interval: call a function repeatedly after a set interval
+let count = 0;
+let cutOffCount = 50;   // cut off interval after 50 calls
+let intervalFn = setInterval(function() {
+  if (true /*some condition that you meet*/) {
+    clearInterval(intervalFn);
+  } else {
+    if (count === cutOffCount) {
+      clearInterval(intervalFn);
+    }
+    count++;
+  }
+}, 500);
+
 // Asynchronous nature and callbacks
 const fs = require('fs');
 fs.readFile('./script.js', function(error, data) {
@@ -18,3 +37,11 @@ Undefined
 
 // convert different base number to integer `parseInt ("<number>", <original base>);`
 parseInt ("4F", 16);
+
+// detect machine / user agent details
+navigator.userAgent   // Full user agent
+navigator.platform    // OS
+navigator.appName     // browser application
+
+// declaring global var in JS
+globalVariable = "Test";
