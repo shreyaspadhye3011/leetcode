c3d86d853ddb99aa76442e36e2e3c2696138b091
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-04-15 00:02:22 -0700

    Added JS filter

--- util/shorthands.js
+++ util/shorthands.js
@@ -1,5 +1,41 @@
 // Reference: https://www.guru99.com/javascript-interview-questions-answers.html
 
+// Filter JS objects
+const data = [
+  {
+    name: 'Bob',
+    gender: 'male',
+    age: 34,
+  },
+  {
+    name: 'Carol',
+    gender: 'female',
+    age: 36,
+  },
+  {
+    name: 'Ted',
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Alice',
+    gender: 'female',
+    age: 40,
+  },
+];
+
+const arr1 = data.filter(d => d.age > 37);
+console.log('arr1', arr1);
+
+const arr2 = data.filter(d => d.gender === 'female');
+console.log('arr2', arr2);
+
+const ageAndGender = d => d.age > 37 && d.gender === 'female';
+
+const arr3 = data.filter(ageAndGender);
+console.log('arr3', arr3);
+
+
 // Timeout: call a function after a delay
 setTimeout(function(){
   // code to exdcute after delay
