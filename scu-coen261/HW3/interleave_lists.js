04c37fd5f1c85e00c3100b8c98018750a03bc088
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-04 14:06:58 -0700

    Added correction for longer second -- use length

--- scu-coen261/HW3/interleave_lists.js
+++ scu-coen261/HW3/interleave_lists.js
@@ -1,38 +1,39 @@
 // Todo: add is_null usage
 function interleave_lists(first, second, interleaved) {
-  // if both empty
-  if (first === null && second === null) { 
-      return interleaved; 
+  display(first);
+// if both empty
+if (first === null && second === null) { 
+    return interleaved; 
+}
+// first still has elements
+else if (second === null) { 
+  interleaved = pair(head(first), interleaved); 
+  return interleave_lists(tail(first), null, interleaved);
+}
+// if both still have elements, interleave
+else {
+  if (interleaved === null) {
+    interleaved = pair(head(first), pair(head(second), null));
+  } else {
+    interleaved = pair(head(first), pair(head(second), interleaved));
   }
-  // first still has elements
-  else if (second === null) { 
-    interleaved = pair(head(first), interleaved); 
-    return interleave_lists(tail(first), null, interleaved);
-  }
-  // if both still have elements, interleave
-  else {
-    if (interleaved === null) {
-      interleaved = pair(head(first), pair(head(second), null));
-    } else {
-      interleaved = pair(head(first), pair(head(second), interleaved));
-    }
-    return interleave_lists(tail(first), tail(second), interleaved);
-  }
+  return interleave_lists(tail(first), tail(second), interleaved);
+}
 }
 
 function interleave_lists_helper(first, second) {
-    let result = [];
-    // always call interleave_lists with first array as larger
-    if (array_length(first) >= array_length(second)) { 
-         result = interleave_lists(reverse(first), reverse(second), null); 
-         return result;
-    } else {
-        result = interleave_lists(reverse(second), reverse(first), null); 
-        return result;
-    }
+  let result = [];
+  // always call interleave_lists with first array as larger
+  if (length(first) >= length(second)) { 
+       result = interleave_lists(reverse(first), reverse(second), null); 
+       return result;
+  } else {
+      result = interleave_lists(reverse(second), reverse(first), null); 
+      return result;
+  }
 }
 
-interleave_lists_helper(list(1,2,3,7), list(4,5,6));
+interleave_lists_helper(list(1,2,3), list(4,5,6, 8));
 
 // let chck = pair(3, pair(1, pair(2, null)));
 // display(chck);
