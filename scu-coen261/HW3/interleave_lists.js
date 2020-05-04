3c3c0aacf2ed92fa9d972a03642731474f1fe82c
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-04 13:56:01 -0700

    Stable: Working end to end!

--- scu-coen261/HW3/interleave_lists.js
+++ scu-coen261/HW3/interleave_lists.js
@@ -2,13 +2,12 @@
 function interleave_lists(first, second, interleaved) {
   // if both empty
   if (first === null && second === null) { 
-      display(interleaved);
       return interleaved; 
   }
   // first still has elements
   else if (second === null) { 
     interleaved = pair(interleaved, head(first)); 
-    interleave_lists(tail(first), null, interleaved);
+    return interleave_lists(tail(first), null, interleaved);
   }
   // if both still have elements, interleave
   else {
@@ -17,8 +16,7 @@ function interleave_lists(first, second, interleaved) {
     } else {
       interleaved = pair(head(first), pair(head(second), interleaved));
     }
-    // display(interleaved);
-    interleave_lists(tail(first), tail(second), interleaved);
+    return interleave_lists(tail(first), tail(second), interleaved);
   }
 }
 
@@ -27,7 +25,6 @@ function interleave_lists_helper(first, second) {
     // always call interleave_lists with first array as larger
     if (array_length(first) >= array_length(second)) { 
          result = interleave_lists(reverse(first), reverse(second), null); 
-         display(result);
          return result;
     } else {
         result = interleave_lists(reverse(second), reverse(first), null); 
