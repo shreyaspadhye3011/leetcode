b935b7db7621cec9a692d012c06a3c90213e5a24
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-04 01:44:11 -0700

    Added working code

--- scu-coen261/HW3/check_overlapping_interval.js
+++ scu-coen261/HW3/check_overlapping_interval.js
@@ -1,3 +1,5 @@
+// Session: https://source-academy.github.io/playground#chap=4&exec=1000&ext=NONE&prgrm=GYVwdgxgLglg9mABAWwIYGsCmB9GYqYBOAbqgDYAUAHgJSIDeiAUIq4oZlCIUgA6oxC1ANoAGALoAaRFWEBGcTQDczAL5NQkWAkRk4AdyLYARnHAATCjDqMWbDlx6IAFplSXrSpus3R4SEF5eI1MLKxtmNnZObiQoAUpPbyYNcD8dCFcIdGw4YiIyVCC8AHNcfCJSMgBnKwqScgB5MExpPAIGsgAVfTgbO1YyTkR2ojlEAF4UDBxRzrqOqubMZQHdYbmAJknprHLF8gXK8h6%2BryiHWN0DELMwD3q5OgAea8NCEzuHjs26NYB%2BRAUPTvT5hLYvRCBYIfUL3I6EJ6IQFQQggTCIABciGA5GqKzW2OBN1hXwRSNe0Nu4Pqv2RiFR6KxOLxKxUySYAHpOYhMphsrl8oRCsUwGU5lVavJNpI5ABmKSIMSSAAsihU3JZNUwXJ5fIFeQKRV4pX2xxqFGVauk0tlCuUiE1uO1ut5WRyhuFxtNErxltEqsV8jt6tYmsZOv1HqFIpNYrNnSlckDNpl9pUUXDaJ1TCAA&variant=default
+
 function make_interval(x) { 
     return pair(x[0], x[1]); 
 }
@@ -7,28 +9,17 @@ function lower_bound(i) {
 function upper_bound(i) { 
     return tail(i);
 }
-function print_interval(i) {
-    return "[ "  + stringify(lower_bound(i)) + 
-           " , " + stringify(upper_bound(i)) + " ]";
-}
-function add_interval(x, y) {
-    return make_interval(lower_bound(x) + lower_bound(y),
-                         upper_bound(x) + upper_bound(y));
-}
-
-
 
 function check_overlapping_intervals(intervalOne, intervalTwo) {
     let inter1 = make_interval(intervalOne);
     let inter2 = make_interval(intervalTwo);
-    // display(print_interval(inter1));
-    // display(print_interval(inter2));
-    return lower_bound(inter2) < upper_bound(inter1) || lower_bound(inter1) < upper_bound(inter2) ? true : false; 
-    // return is_overlap(array[i]) ? true : check_overlapping_interval(array, i+1);
+    return lower_bound(inter1) < lower_bound(inter2) 
+    ? (lower_bound(inter2) < upper_bound(inter1) ? true : false)
+    : (lower_bound(inter1) < upper_bound(inter2) ? true : false); 
 }
 
-check_overlapping_intervals([1,3], [2,4]);
-
-// print_interval(add_interval(make_interval(1, 2), 
-//                             make_interval(3, 5)));
+// check_overlapping_intervals([12,13], [0,4]); // false
+// check_overlapping_intervals([0,4], [12,13]); // false
+// check_overlapping_intervals([0,4], [1,13]);  // true
+check_overlapping_intervals([1,4], [2,3]);     // true
 
