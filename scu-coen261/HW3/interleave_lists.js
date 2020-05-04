cd1fb78e0b6377b55f913a70a2b5a4a39041f8c3
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-04 09:38:19 -0700

    Working interleave lists

---
+++ scu-coen261/HW3/interleave_lists.js
@@ -0,0 +1,24 @@
+function interleave_lists(first, second, interleaved) {
+  // Todo: Always call with second as smaller
+  if (first !== null && second !== null && array_length(second) > array_length(first)) { interleave_lists(second, first); }
+  else {    
+      // if both empty
+      if (first === null && second === null) { return interleaved; }
+      // first still has elements
+      else if (second === null) { 
+        interleaved = pair(interleaved, head(first)); 
+        interleave_lists(tail(first), null, interleaved);
+      }
+      // if both still have elements, interleave
+      else {
+        if (interleaved === null) {
+          interleaved = pair(interleaved, pair(head(first), head(second)));
+        } else {
+          interleaved = pair(head(first), pair(head(second), null));
+        }
+        interleave_lists(tail(first), tail(second), interleaved);
+      }
+  }
+}
+
+interleave_lists(list(1,2,3), list(4,5,6), null);
