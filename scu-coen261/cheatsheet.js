dcfbd2bd2db9860eaefec7e9a87bff768fd60905
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-04 20:01:40 -0700

    added cheatsheet

--- scu-coen261/cheatsheet.js
+++ scu-coen261/cheatsheet.js
@@ -2,5 +2,12 @@
 for_each(x => display(x), 
          list(57, 321, 88));
 
+// list creation - 1
 let chck = pair(3, pair(1, pair(2, null)));     // [3, [1, [2, null]]]
 display(chck);
+
+// list creation - 2
+list(1,2,3);      // [1, [2, [3, null]]]
+
+// list append
+append([1, [4, null]],pair(2, null));   // [1, [4, [2, null]]]
