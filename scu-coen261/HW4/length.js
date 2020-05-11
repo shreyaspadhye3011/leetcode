dad8ee9fa2cc64fcbabdfd180cd13d8242ecc147
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-10 18:38:26 -0700

    Working length code

--- scu-coen261/HW4/length.js
+++ scu-coen261/HW4/length.js
@@ -1,6 +1,4 @@
-function foldr(op, init, xs) {
-    return is_null(xs) ? init : op(head(xs), foldr(op, init, tail(xs))); }
-  
-  function length(xs) { return foldr((x, n) => n+1, 0, xs); }
-  
-  length([2, [3, [4, [4, null]]]]);
+// https://tinyurl.com/y8au9jum
+const foldr = op => init => xs => is_null(xs) ? init : op(head(xs), foldr(op)(init)(tail(xs)));
+const length = (xs) => foldr((xs,n) => n+1)(0)(xs); 
+length([2, [3, [4, [4, null]]]]);
