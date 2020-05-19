5fbcee2f7bec5ca9346b59a3af3ce3bcd9046f5e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-18 20:11:35 -0700

    Some parts Working code

--- scu-coen261/HW5 - Dictionary/update_dict.js
+++ scu-coen261/HW5 - Dictionary/update_dict.js
@@ -1,6 +1,7 @@
 const key = head;
 const value = tail;
-const insert_dict = (k, v) => dict => pair(pair(k, v), dict);
+
+const insert_dict = (item,dict) => pair(item, dict);
 
 function build_dict(list) {
   return is_null(list) ? null : insert_dict(head(list), build_dict(tail(list)));
@@ -8,9 +9,23 @@ function build_dict(list) {
 
 const lookup = dict => k => !is_null(dict) && 
                             (key(head(dict)) === k ? value(head(dict)) :
-                                                     lookup(tail(dict), k));
-// const update_dict = dict => (k, v) => ????????
+                                                     lookup(tail(dict))(k));
 
+// const update_value = dict => k => v => !is_null(dict) && 
+//                             (key(head(dict)) === k ? value(head(dict)) :
+//                                                      lookup(tail(dict)(k)));
+
+const build_updated_dict = dict => (k, v) => {
+  return is_null(dict) ? null 
+  : key(head(dict)) === k
+    ? insert_dict(pair(k, v), build_dict(tail(dict))) 
+    : insert_dict(head(dict), build_dict(tail(dict)));
+};
+                                                     
+                                                     
+// const update_dict = dict => (k, v) => lookup(dict)(k) ? update_value(dict)(k)(v) : insert_dict(k, v)(dict);
 const hungarian = build_dict(list(pair(1, "egy"), pair(2, "ketto"), pair(3, "harom")));
 
-hungarian(3);
+// hungarian;
+// lookup(hungarian)(7);
+build_updated_dict(hungarian)(3, "milo");
