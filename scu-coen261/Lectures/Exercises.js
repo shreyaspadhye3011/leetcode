f9d0bfc8cb75d4dfbabf8e0bd2815bf97416a8f7
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-30 15:11:09 -0700

    Exercise1.3 done

--- scu-coen261/Lectures/Exercises.js
+++ scu-coen261/Lectures/Exercises.js
@@ -6,8 +6,13 @@ const sum_of_squares = a => b => square(a) + square(b);
 
 sum_of_squares(2)(3);
 
-// const sum_larger_sqaures = a => b => c 
-// => (a > b && b > c) ? sum_of_squares(a)(b);
-// sum_larger_sqaures(2);
+const sum_larger_sqaures = a => b => c => 
+(a >= b && b >= c) 
+? sum_of_squares(a)(b)
+: (b >= c && c >= a) 
+? sum_of_squares(b)(c)
+: sum_of_squares(a)(c);
+
+sum_larger_sqaures(3)(3)(2);
 
 ////// ********************** //////
