96f256a0c8df315139c8c278b9cd18fc1bc96eac
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-05-30 16:42:37 -0700

    Added an important exercise regarding Applicative / Normal order

--- scu-coen261/Lectures/Exercises.js
+++ scu-coen261/Lectures/Exercises.js
@@ -1,11 +1,10 @@
 // Sol: Exercise 1.3 SICP JS
 
 const square = a => a*a;
-
 const sum_of_squares = a => b => square(a) + square(b); 
-
 sum_of_squares(2)(3);
 
+// return sum of squares for two larger numbers
 const sum_larger_sqaures = a => b => c => 
 (a >= b && b >= c) 
 ? sum_of_squares(a)(b)
@@ -16,3 +15,17 @@ const sum_larger_sqaures = a => b => c =>
 sum_larger_sqaures(3)(3)(2);
 
 ////// ********************** //////
+
+// Exercise 1.5 SICP JS
+function p() {
+    return p();
+}
+
+function test(x, y) {
+    return x === 0 ? 0 : y;
+}
+
+test(0, p());   // Source / JS is applicative order as it results in infinte loop. Normal order wouldn't even need to evaluate p() as ternary will result in 0. -- Check solution in book if confused
+
+// Normal order: Returns 0
+// Applicative order: infinte loop
--- scu-coen261/Lectures/Lecture 1.js
+++ scu-coen261/Lectures/Lecture 1.js
@@ -51,6 +51,20 @@ const a = (x) => (y) => square(x) + square(y);
 
 a(3)(3);
 
+// Exercise 1.5 SICP JS
+function p() {
+    return p();
+}
+
+function test(x, y) {
+    return x === 0 ? 0 : y;
+}
+
+test(0, p());   // Source / JS is applicative order as it results in infinte loop. Normal order wouldn't even need to evaluate p() as ternary will result in 0. -- Check solution in book if confused
+
+// Normal order: Returns 0
+// Applicative order: infinte loop
+
 
 
   
