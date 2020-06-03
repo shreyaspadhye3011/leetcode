5e189c62a21f4ecd8b7f6804f315e0570db0875f
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-03 16:11:47 -0700

    Completed double factorial

--- scu-coen261/Finals/q9_double_factorial.js
+++ scu-coen261/Finals/q9_double_factorial.js
@@ -1,6 +1,18 @@
+// sourceacademy: https://api2.sourceacademy.nus.edu.sg/doublefacto
+// Accepts a number and returns double factorial as described here: https://mathworld.wolfram.com/DoubleFactorial.html
+// Author: Shreyas Padhye, spadhye@scu.edu, 00001556287
 const double_factorial = n => {
     const double_fact_iter = (num, product) => {
-        return num === 0 || num === -1 ? product : double_fact_iter(num-2, num*product);
+        return num < -1 ? "not defined" : (num === 0 || num === -1) ? product : double_fact_iter(num-2, num*product);
     };
     return double_fact_iter(n, 1);
 };
+
+// tests
+display(double_factorial(-1));      // 1
+display(double_factorial(4));       // 8
+display(double_factorial(5));       // 15
+display(double_factorial(6));       // 48
+display(double_factorial(7));       // 105
+double_factorial(-7);               // not defined
+
