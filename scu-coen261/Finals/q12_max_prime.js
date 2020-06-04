21142a96c037610ad06281b58d2ced718fa29348
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-03 21:50:18 -0700

    Added helper for max_prime and ans for calculate_lcm

--- scu-coen261/Finals/q12_max_prime.js
+++ scu-coen261/Finals/q12_max_prime.js
@@ -1,7 +1,10 @@
 // Author: Shreyas Padhye, spadhye@scu.edu, 00001556287
+// sourceacademy: https://api2.sourceacademy.nus.edu.sg/largestprimefactor
 
+// keep dividing a given number with a divisor until it can
 const remove_all = n => divisor => n % divisor !== 0 ? n : remove_all(math_floor(n/divisor))(divisor);
 
+// retrieve largest prime factor for a number
 const get_max_prime_factor = n => current_divisor => max_prime => {
     return n === 0
     ? max_prime
@@ -11,5 +14,7 @@ const get_max_prime_factor = n => current_divisor => max_prime => {
     ? get_max_prime_factor(remove_all(n)(current_divisor))(current_divisor+1)(current_divisor)
     : get_max_prime_factor(n)(current_divisor+1)(max_prime);
 };
-// todo: use helper to pass init values
-get_max_prime_factor(600851475143)(2)(2);     // 6857
+
+const max_prime_factor = n => get_max_prime_factor(n)(2)(2);
+
+max_prime_factor(600851475143);     // 6857
