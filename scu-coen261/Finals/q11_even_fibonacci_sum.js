d3385f76d3f783e980f694cc7053307a6aa21d24
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-03 18:47:21 -0700

    sum each working but for low values

--- scu-coen261/Finals/q11_even_fibonacci_sum.js
+++ scu-coen261/Finals/q11_even_fibonacci_sum.js
@@ -7,6 +7,7 @@ const take = n => s => is_null(s) || n <= 0 ? null :
                                               pair(head(s), () => take(n-1)(stream_tail(s)));
 
 const foreach = f => xs => is_null(xs) ? true : KI(f(head(xs)))(foreach(f)(stream_tail(xs)));
+const sum_each = xs => sum => is_null(xs) ? sum : sum_each(stream_tail(xs))(sum+head(xs));
 const display_stream = foreach(display);
 
 const filter = p => s => is_null(s) ? null :
@@ -16,9 +17,8 @@ const filter = p => s => is_null(s) ? null :
 
 const fibgen = (a,b) => pair(a, () => fibgen(b, a+b));
 const fibs = fibgen(0, 1);
-display_stream(take(18)(fibs));
-
-display("--------------");
+// display_stream(take(18)(fibs));
+// display("--------------");
 
 const is_even = a => a%2 === 0;
 function sieve(stream) {
@@ -29,3 +29,4 @@ const even_fibs = sieve(fibs);
 
 even_fibs;
 display_stream(take(8)(even_fibs));
+sum_each(take(100)(even_fibs))(0);
