7d4d220849cb701ab1c342dbefa696093763ac2f
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-04 00:34:27 -0700

    Working LCM

--- scu-coen261/Finals/q13_calculate_lcm.js
+++ scu-coen261/Finals/q13_calculate_lcm.js
@@ -1,4 +1,6 @@
 // 232,792,560
+// 232792560
+
 
 // xs - 20, 19, 18, 17, 16, 15, 14, 13, 12, 11
 
@@ -16,3 +18,12 @@ const number_list = list(20, 19, 18, 17, 16, 15, 14, 13, 12, 11);
 number_list;
 head(number_list);  // 20
 tail(number_list);  // [19, [18, [17, [16, [15, [14, [13, [12, [11, null]]]]]]]]]
+
+const get_value = xs => is_null(xs) ? null : head(xs);
+const next = xs => is_null(xs) ? null : tail(xs);
+const find_lcm = xs => current_lcm => is_null(next(xs))
+                                        ? current_lcm
+                                        : find_lcm(next(xs))(lcm(current_lcm)(get_value(next(xs))));
+                                        
+    
+find_lcm(number_list)(20);      // 232792560
