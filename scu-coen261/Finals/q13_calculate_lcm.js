9be8fb6bfbad32ae8aa6f1a7d072ba6842063c6e
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-04 15:02:55 -0700

    Added end to end working LCM coed

--- scu-coen261/Finals/q13_calculate_lcm.js
+++ scu-coen261/Finals/q13_calculate_lcm.js
@@ -1,29 +1,21 @@
-// 232,792,560
-// 232792560
+// Author: Shreyas Padhye, spadhye@scu.edu, 00001556287
+// sourceacademy: https://api2.sourceacademy.nus.edu.sg/lcm
 
-
-// xs - 20, 19, 18, 17, 16, 15, 14, 13, 12, 11
-
-// for every pair of numbers from back, get lcm(head(xs), head(tail(xs))) and pass it ahead into the list
-
+// get lcm of two numbers
 const lcm = a => b => a >= b ? lcm_helper(a)(b)(1) : lcm_helper(b)(a)(1);
 const is_divisible = a => b => a % b === 0;
 const lcm_helper = a => b => multiplier => is_divisible(a*multiplier)(b) 
                                             ? a*multiplier : lcm_helper(a)(b)(multiplier+1);
                                             
 
-// todo: create this list dynamically
-// any number that is divisible by these will definitely be divisible by 1-10
+// any number that is divisible by these will definitely be divisible by 1-10. Hence only these considered in calculation
 const number_list = list(20, 19, 18, 17, 16, 15, 14, 13, 12, 11);
-number_list;
-head(number_list);  // 20
-tail(number_list);  // [19, [18, [17, [16, [15, [14, [13, [12, [11, null]]]]]]]]]
-
 const get_value = xs => is_null(xs) ? null : head(xs);
 const next = xs => is_null(xs) ? null : tail(xs);
 const find_lcm = xs => current_lcm => is_null(next(xs))
                                         ? current_lcm
                                         : find_lcm(next(xs))(lcm(current_lcm)(get_value(next(xs))));
                                         
-    
-find_lcm(number_list)(20);      // 232792560
+
+const get_lcm = mun_list => find_lcm(mun_list)(20);
+get_lcm(number_list);      // 232792560
