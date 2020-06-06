f7e860cc25225b9af81fb2ab736bfed6c1ecf58d
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-05 22:29:49 -0700

    Added error handling

--- scu-coen261/Finals/q11_even_fibonacci_sum.js
+++ scu-coen261/Finals/q11_even_fibonacci_sum.js
@@ -1,8 +1,9 @@
+
 const fib = n => {
   const fib_iter = n => a => b => count => {
       return count === n ? b : fib_iter(n)(b)(a+b)(count+1);
   };
-  return n === 1 ? 0 : fib_iter(n)(0)(1)(2);  
+  return n < 1 ? "Not defined." : n === 1 ? 1 : fib_iter(n)(1)(2)(2);  
 };
 
-fib(4);
+fib(0);
