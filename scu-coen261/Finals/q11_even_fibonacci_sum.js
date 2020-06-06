03617e871fdc1c669cfc739b7084fb46b865639a
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-05 22:43:31 -0700

    Cleaned up fib_sum and added tests

--- scu-coen261/Finals/q11_even_fibonacci_sum.js
+++ scu-coen261/Finals/q11_even_fibonacci_sum.js
@@ -9,14 +9,19 @@
 // fib(0);
 
 
-// return sum of all even valued terms in the first n numbers of the sequence
+// returns sum of all even valued terms in the first n numbers of the sequence
 const fib_sum = n => {
   const is_even = num => num % 2 === 0;
   const fib_sum_iter = n => a => b => count => sum => {
     const updated_sum = is_even(b) ? sum + b : sum;
     return count === n ? updated_sum : fib_sum_iter(n)(b)(a+b)(count+1)(updated_sum);
   };
-  return n <= 1 ? 0 : n === 2 ? 2 : fib_sum_iter(n)(1)(2)(2)(0);  
+  return n <= 1 ? 0 : fib_sum_iter(n)(1)(2)(2)(0);  
 };
 
-fib_sum(5);
+// tests
+// fib_sum(1);      // 0
+// fib_sum(2);      // 2
+// fib_sum(5);      // 10
+// fib_sum(7);      // 10
+// fib_sum(10);     // 44
