415e4f4c4ed3f919a1789a521a6e3bc04bf7e2ae
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-05 22:50:40 -0700

    Added working fibonacci sequence

--- scu-coen261/Finals/q11_even_fibonacci_sum.js
+++ scu-coen261/Finals/q11_even_fibonacci_sum.js
@@ -9,19 +9,21 @@
 // fib(0);
 
 
-// returns sum of all even valued terms in the first n numbers of the sequence
+// returns sum of all even valued terms in the first n numbers of the fibonacci sequence
 const fib_sum = n => {
   const is_even = num => num % 2 === 0;
   const fib_sum_iter = n => a => b => count => sum => {
     const updated_sum = is_even(b) ? sum + b : sum;
-    return count === n ? updated_sum : fib_sum_iter(n)(b)(a+b)(count+1)(updated_sum);
+    return count === n || updated_sum === Infinity ? updated_sum : fib_sum_iter(n)(b)(a+b)(count+1)(updated_sum);
   };
   return n <= 1 ? 0 : fib_sum_iter(n)(1)(2)(2)(0);  
 };
 
 // tests
-// fib_sum(1);      // 0
-// fib_sum(2);      // 2
-// fib_sum(5);      // 10
-// fib_sum(7);      // 10
-// fib_sum(10);     // 44
+display(fib_sum(1));        // 0
+display(fib_sum(2));        // 2
+display(fib_sum(5));        // 10
+display(fib_sum(7));        // 10
+display(fib_sum(10));       // 44
+fib_sum(4000000);           // Infinity
+
