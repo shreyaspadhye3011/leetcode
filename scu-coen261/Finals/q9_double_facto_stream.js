2aa2a77148826df3d766d4fc8fd9aad61e298939
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-08 11:49:30 -0700

    Added double factorial code

--- scu-coen261/Finals/q9_double_facto_stream.js
+++ scu-coen261/Finals/q9_double_facto_stream.js
@@ -1,5 +1,6 @@
 // Author: Shreyas Padhye, spadhye@scu.edu, 00001556287
 // Courtesy: Prof. Vlad Patryshev & SICP JS
+// sourceacademy: https://api2.sourceacademy.nus.edu.sg/doublefactorialstream
 
 // stream basics
 const KI = a => b => b;
@@ -12,6 +13,7 @@ const foreach = f => xs => is_null(xs) ? "." : KI(f(head(xs)))(foreach(f)(stream
 const display_stream = foreach(display);
 
 // stream generator for double factorial based on https://mathworld.wolfram.com/DoubleFactorial.html 
+// optimize? -- refer fib_gen (slides) and see if you can do an implementation like that. Here double_factorial_gen duplicates are called for every number. You can pass double_factorial_gen(n-2) value as an argument instead of passing everytime
 const double_factorial_gen = n => (n === 0 || n === -1) ? pair(1, null) : pair(n * head(double_factorial_gen(n-2)), () => double_factorial_gen(n+1));
 const double_factorial = double_factorial_gen(1);
 
