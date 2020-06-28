92e07b3816f664240c66e79152fd9e851e685e2c
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-27 23:14:55 -0700

    Changed name to wordList

--- util/class_design_basics.py
+++ util/class_design_basics.py
@@ -1,8 +1,8 @@
 # https://docs.python.org/3/tutorial/classes.html
 
-class Anagram:    
+class WordObject:    
   # class variable to store all anagram objects
-  anagrams = [] 
+  words = [] 
 
   # default constructor - added for completeness. Not required until you want to initialize some instance parameter as default constructor is inherently defined for you. Comment this and test. it will work
   def __init__(self):
@@ -13,16 +13,17 @@ class Anagram:
   def create_objects_from_list(self, arr_list):
     for index,value in enumerate(arr_list):
       # deafault constructor usage
-      obj = Anagram()
+      obj = WordObject()
       obj.value = value
       obj.index = index
-      self.anagrams.append(obj)
-    return self.anagrams
+      self.words.append(obj)
+    return self.words
 
-obj = Anagram()
-anagram_list = obj.create_objects_from_list(["ace", "cea", "eca"])
+obj = WordObject()
+word_list = obj.create_objects_from_list(["ace", "cea", "ceca"])
 
-for obj in anagram_list:
+for obj in word_list:
   print(obj.index, obj.value)
+
 
 
