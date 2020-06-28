33a1e84dd3eed6e00be5a38ec7a9a87225b7d79c
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-27 23:22:01 -0700

    Added custom sort

--- util/class_design_basics.py
+++ util/class_design_basics.py
@@ -1,7 +1,8 @@
 # https://docs.python.org/3/tutorial/classes.html
 
+# basics
 class WordObject:    
-  # class variable to store all anagram objects
+  # class variable to store all word objects
   words = [] 
 
   # default constructor - added for completeness. Not required until you want to initialize some instance parameter as default constructor is inherently defined for you. Comment this and test. it will work
@@ -20,10 +21,22 @@ class WordObject:
     return self.words
 
 obj = WordObject()
-word_list = obj.create_objects_from_list(["ace", "cea", "ceca"])
+word_list = obj.create_objects_from_list(["yace", "cea", "aeca"])
 
 for obj in word_list:
-  print(obj.index, obj.value)
+  print(obj.index, obj.value, type(obj))
+# 0 yace <class '__main__.WordObject'>
+# 1 cea <class '__main__.WordObject'>
+# 2 aeca <class '__main__.WordObject'>
+
+# sorting a list of objects
+word_list.sort(key=lambda x: x.value)
+
+for obj in word_list:
+  print(obj.index, obj.value, type(obj))
+# 2 aeca <class '__main__.WordObject'>
+# 1 cea <class '__main__.WordObject'>
+# 0 yace <class '__main__.WordObject'>
 
 
 
