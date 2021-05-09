124ca4ed6d629da9c47223fe8d4aacf36a6ace5d
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2021-05-09 15:59:43 -0700

    Added gitignore and cd project

---
+++ C#/Codecademy/src/Program.cs
@@ -0,0 +1,12 @@
+﻿// using System;
+
+// namespace Codecademy
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
